e10712420bf583c74747ae0eb723e7f5ccb8e950
Santhosh Balu <chenkarai@gmail.com>
2021-09-10 17:58:46 +1000

    upload react-tasks sample project

---
+++ src/components/Task.js
@@ -0,0 +1,17 @@
+
+const Task = ({ task, onDelete, onToggle }) => {
+  return (
+    <div
+      className={`task ${task.reminder && 'reminder'}`}
+      onDoubleClick={() => onToggle(task.id)}
+    >
+      <h3>
+        {task.text}{' '}
+        <button onClick={() => onDelete(task.id)}>X</button>
+      </h3>
+      <p>{task.day}</p>
+    </div>
+  )
+}
+
+export default Task
